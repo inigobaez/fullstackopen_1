710357dfb50f368dcbf3fe0ca66a9ac5cdf13648
Iñigo Baez Gurruchaga <inigobaezgurruchaga@MacBook-Air-de-Inigo.local>
2023-12-05 16:10:18 +0100

    exercise 1.5

--- course_info/src/App.jsx
+++ course_info/src/App.jsx
@@ -1,34 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Header = ({ course }) => {
+  return (<h1>{course}</h1>);
+}
+const Total = ({ parts }) => {
+  const total = parts.reduce((acc, current) => acc + current.exercises, 0)
 
-function App() {
-  const [count, setCount] = useState(0)
-
+  return (<p>Number of exercises {total}</p>)
+}
+const Part = ({ name, exercises }) => {
+  return (<p>
+    {name} {exercises}
+  </p>)
+}
+const Content = ({ parts }) => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {parts.map((p, idx) => <Part key={idx} name={p.name} exercises={p.exercises} />)}
     </>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+
+      <Total parts={course.parts} />
+    </div>
   )
 }
 
